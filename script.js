c9bc1669fca0219816122f2d887b1f02c5ba8f37
Rrr <83889502+WolfGamer2@users.noreply.github.com>
2024-11-06 20:17:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -31,3 +31,10 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+<script>
+  window.addEventListener("load", (event) => {
+    new cursoreffects.springyEmojiCursor({ emoji: "🤷‍♂️" });
+  });
+</script>
+
